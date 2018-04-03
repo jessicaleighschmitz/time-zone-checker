22a21c27bcdb5d7f58f05b07a6dc5966da8983a4
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-03 14:03:19 -0700

    add working js clock

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 
-const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
   const now = new Date();
-
+  const secondHand = document.querySelector('.second-hand');
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds/60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   console.log(seconds);
+
+  const minuteHand = document.querySelector('.minute-hand');
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 6) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hourHand = document.querySelector('.hour-hand');
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 setInterval(setDate, 1000);
 
